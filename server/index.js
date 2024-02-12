c650e976f564e068ad5c25414ae6843e0c311360
anishkt200 <anishkt20@gmail.com>
2024-02-12 11:29:44 +0530

    deployment req

--- server/index.js
+++ server/index.js
@@ -7,16 +7,31 @@ const userRoutes = require("./Routes/userRoutes");
 const messages = require("./Routes/messages");
 const connectDb = require("./utils/db");
 var morgan = require("morgan");
+const path= require("path");
 app.use(cors());
 app.use(express.json());
 const socket = require("socket.io");
-
+const _dirname =path.dirname("");
+const buildPath =path.join(_dirname,"../let's talk/dist");
+app.use(express.static(buildPath));
 app.use(morgan("tiny"));
 app.use("/api/auth", userRoutes);
 app.use("/api/messages", messages);
 app.use("/", (req, res) => {
   res.send([]);
 });
+app.get("/*", function(req, res){
+
+  res.sendFile(
+      path.join(__dirname, "../let's talk/dist/index.html"),
+      function (err) {
+        if (err) {
+          res.status(500).send(err);
+        }
+      }
+    );
+
+})
 
 connectDb();
 const server = app.listen(process.env.PORT, () =>
